1fc20c915c2e75a6ef689fa0d6d9d04de072246b
yeeecheng <yichengliao1003@gmail.com>
2023-01-01 16:09:36 +0800

    add file

---
+++ prepare.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/ultralytics/yolov5.git
+mv best-int8_edgetpu.tflite  ./yolov5
+mv face_detect.py  ./yolov5
+
+echo "names:
+- face
+nc: 1
+train: $(pwd)/dataset/face_detect_dataset/images/train
+val: $(pwd)/dataset/face_detect_dataset/images/val
+"  >  dataset.yaml
+
