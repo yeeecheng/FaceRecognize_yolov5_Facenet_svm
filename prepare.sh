1fbc6c384417b10b1350fc93068918ab37290583
Yui-Arthur <arthur80633@gmail.com>
2023-01-02 01:23:24 +0800

    fix bug

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,9 @@
 git clone https://github.com/ultralytics/yolov5.git
-mv best-int8_edgetpu.tflite  ./yolov5
-mv face_detect.py  ./yolov5
-
+mv best-int8_edgetpu.tflite ./yolov5
+mv face_detect.py ./yolov5
 echo "names:
 - face
 nc: 1
 train: $(pwd)/dataset/face_detect_dataset/images/train
 val: $(pwd)/dataset/face_detect_dataset/images/val
 "  >  dataset.yaml
-
